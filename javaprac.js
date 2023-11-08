d69caa84be5439cdebd99e6a81e1121c3f6f7cc3
teocastellvi <teo.castellvi.7e7@itb.cat>
2023-11-08 09:42:35 +0100

    ewrwqe

--- javaprac.js
+++ javaprac.js
@@ -1,74 +1,79 @@
+var intents = 0;
+var lletresCorrectes = 0;
+var numerorandomSplit;
+
 // Definir la función para generar un número aleatorio
 function generarNumeroAleatorio() {
-    // Generar un número aleatorio entre 0 y 99999
-    var numeroAleatorio = Math.floor(Math.random() * 100000);
-    // Rellenar con ceros al inicio si el número tiene menos de 5 cifras
-    numeroAleatorio = numeroAleatorio.toString().padStart(5, '0');
-    return numeroAleatorio;
+  // Generar un número aleatorio entre 0 y 99999
+  var numeroAleatorio = Math.floor(Math.random() * 100000);
+  // Rellenar con ceros al inicio si el número tiene menos de 5 cifras
+  numeroAleatorio = numeroAleatorio.toString().padStart(5, '0');
+  return numeroAleatorio;
 }
 
 // Llamar a la función cuando se cargue el documento
 document.addEventListener("DOMContentLoaded", function() {
-    var numeroRandom = generarNumeroAleatorio();
-    numerorandomSplit = numeroRandom.split("")
-    console.log(numeroRandom);
+  var numeroRandom = generarNumeroAleatorio();
+  numerorandomSplit = numeroRandom.split("");
+  console.log(numeroRandom);
+  console.log(numerorandomSplit);
 });
 
-var intents = 0
-var lletresCorrectes = 0
-
 function comprobar() {
-    lletresCorrectes = 0
+  lletresCorrectes = 0;
 
-    if(intents< 5){
+  if (intents < 5) {
     var numero = document.getElementById("inputdiv3").value;
-    let numeroArray = numero.split('')
+    var numeroArray = numero.split('');
 
-        for (let i = 0; i < 5; i++) {
-        const newDiv = document.createElement("div");
-        newDiv.className = "cuadrados";
-        newDiv.innerHTML = numeroArray[i] 
-        document.getElementById("div5").appendChild(newDiv);
+    for (var i = 0; i < 5; i++) {
+      var newDiv = document.createElement("div");
+      newDiv.className = "cuadrados";
+      newDiv.innerHTML = numeroArray[i];
+      document.getElementById("div5").appendChild(newDiv);
 
-        if(numeroArray[i] === numerorandomSplit[i]){
-            newDiv.style.backgroundColor = "green"
-            lletresCorrectes++
+      if (numeroArray[i] === numerorandomSplit[i]) {
+        newDiv.style.backgroundColor = "green";
+        lletresCorrectes++;
+      } else {
+        for (var j = 0; j < 5; j++) {
+          if (numeroArray[i] === numerorandomSplit[j]) {
+            newDiv.style.backgroundColor = "yellow";
+          }
         }
-        else {
-            for(let j = 0; j < 5; j++){
-                if(numeroArray[i] === numerorandomSplit[j]) newDiv.style.backgroundColor = "yellow"
-            }
+      }
+    }
+    intents++;
+
+    if (lletresCorrectes === 5) {
+      document.getElementById("div4").innerHTML = "Has guanyaaaaat!!!!";
+      intents = 5;
+      document.getElementById("div4").style.backgroundColor = "green";
+      for (var i = 0; i < 5; i++) {
+        document.getElementById("divesteriscos" + (i + 1)).innerHTML = numerorandomSplit[i];
+      }
+    } else{
+        document.getElementById("div4").style.backgroundColor = "red";
         }
-        }
-        intents++
-        if(lletresCorrectes === 5){
-            document.getElementById("div4").innerHTML = "Has gunyaaaaat!!!!";
-            intents = 5
-            document.getElementById("div4").style.backgroundColor = "green"
+  }
 
-        } else{
-
-        switch(intents){
-        case 1:
-            document.getElementById("div4").innerHTML = "Primer intent, sort!";
-            break;
-            case 2:
-            document.getElementById("div4").innerHTML = "Segon intent, sort!";
-            break;
-            case 3:
-            document.getElementById("div4").innerHTML = "tercer intent, sort!";
-            break;
-            case 4:
-            document.getElementById("div4").innerHTML = "Cuart intent, sort!"
-            break;
-            case 5:
-            document.getElementById("div4").innerHTML = "Ultim intent, sort!"
-            break;
-            default:
-                document.getElementById("div4").innerHTML = "Ja has acabat els teus intents!"
-        }}
-} 
-
+  switch (intents) {
+    case 1:
+      document.getElementById("div4").innerHTML = "Primer intent, sort!";
+      break;
+    case 2:
+      document.getElementById("div4").innerHTML = "Segon intent, sort!";
+      break;
+    case 3:
+      document.getElementById("div4").innerHTML = "Tercer intent, sort!";
+      break;
+    case 4:
+      document.getElementById("div4").innerHTML = "Cuart intent, sort!";
+      break;
+    case 5:
+      document.getElementById("div4").innerHTML = "Ja has acabat els teus intents!";
+      break;
+    default:
+      document.getElementById("div4").innerHTML = "Ja has acabat els teus intents!";
+  }
 }
-    
-    
